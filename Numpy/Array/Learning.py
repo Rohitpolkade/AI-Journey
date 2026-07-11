74c6d629dada587f7c76ca494bff1384c5c39572
Rohit <rohitpolkade325@gmail.com>
2026-07-11 16:56:43 +0530

    Update NumPy basics

--- Numpy/Array/Learning.py
+++ Numpy/Array/Learning.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 marks = np.array([10, 20, 30, 40, 50])
-# print("marks:", marks)
-# print(type(marks))
+print("marks:", marks)
+print(type(marks))
 
 """operation -- vectorization"""
-# print(marks + 15)
-# print(marks * 2)
-# print(marks / 5)
+print(marks + 15)
+print(marks * 2)
+print(marks / 5)
 
 """functions -- statistics"""
-# print(np.mean(marks))
-# print(np.max(marks))
-# print(np.min(marks))
-# print(np.sum(marks))
-# # var - variance
-# print(np.var(marks))
-# # std - standard deviation
-# print(np.std(marks)) 
+print(np.mean(marks))
+print(np.max(marks))
+print(np.min(marks))
+print(np.sum(marks))
+# var - variance
+print(np.var(marks))
+# std - standard deviation
+print(np.std(marks)) 
 
 """Filtering -- masking"""
 print(marks > 30)
